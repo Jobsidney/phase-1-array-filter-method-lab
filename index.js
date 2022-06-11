36c6546c62c0c98327622ad8ac8485d6c37fdbc7
Jobsidney <jobsidney67@gmail.com>
2022-06-11 11:17:18 +0300

    commit tree d8837b7ba3dbeaba185904d211dac6e87140c137

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution here
+// const drivers = [
+//     {
+//       name: 'Bobby',
+//       hometown: 'Pittsburgh' },
+//     {
+//       name: 'Sammy',
+//       hometown: 'New York' } ,
+//     {
+//       name: 'Sally',
+//       hometown: 'Cleveland' },
+//     {
+//       name: 'Annette',
+//       hometown: 'Los Angeles' },
+//     {
+//       name: 'Bobby',
+//       hometown: 'Tampa Bay' }
+//   ];
+
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+const findMatching=drivers=>drivers.filter(driver => driver=='Bobby')
+console.log(findMatching(drivers));
